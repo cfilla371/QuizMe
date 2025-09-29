6767d120c736b1b80e665db77a78dbe9d9251c7f
cfilla371 <cfilla371@gmail.com>
2025-09-29 13:24:09 -0500

    Account creation updates

--- QuizMe/src/main/java/com/Corey/QuizMe/controllers/UserController.java
+++ QuizMe/src/main/java/com/Corey/QuizMe/controllers/UserController.java
@@ -1,6 +1,8 @@
 package com.Corey.QuizMe.controllers;
 
 import com.Corey.QuizMe.models.User;
+import com.Corey.QuizMe.services.AccountRegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final AccountRegisterService accountRegisterService;
+
+    @Autowired
+    public UserController(AccountRegisterService accountRegisterService){
+        this.accountRegisterService = accountRegisterService;
+    }
+
     @PostMapping("/account/register")
     public User accountRegister(@RequestBody User user) {
-
+        return accountRegisterService.createUser(user);
     };
 
-    @PostMapping("/account/update")
-    public User accountUpdate(@RequestBody User user) {
-
-    };
+//    @PostMapping("/account/update")
+//    public User accountUpdate(@RequestBody User user) {
+//
+//    };
 
 }
--- QuizMe/src/main/java/com/Corey/QuizMe/services/AccountRegisterService.java
+++ QuizMe/src/main/java/com/Corey/QuizMe/services/AccountRegisterService.java
@@ -1,4 +1,21 @@
 package com.Corey.QuizMe.services;
 
+import com.Corey.QuizMe.models.User;
+import com.Corey.QuizMe.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountRegisterService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AccountRegisterService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
 }
